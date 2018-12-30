5653550e42e9b64935c6c7b35ebbc4d15242b2ca
Andres Baumeler <andres.baumeler@gmail.com>
2018-12-30 18:07:24 +0100

    changed paths to relative

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var browserSync = require('browser-sync').create("server1");
 var sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'docs/scss/*.scss'])
         .pipe(sass())
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
@@ -11,18 +11,18 @@ gulp.task('sass', function(){
 
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest("src/js"))
+        .pipe(gulp.dest("docs/js"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('serve', function(done) {
     browserSync.init({
-        server: "./src"  
+        server: "./docs"  
     });
 
     gulp.watch('node_modules/bootstrap/scss/bootstrap.scss', gulp.series('sass'));
-    gulp.watch('src/scss/*.scss', gulp.series('sass'));
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch('docs/scss/*.scss', gulp.series('sass'));
+    gulp.watch("docs/*.html").on('change', browserSync.reload);
     done();
 });
 
